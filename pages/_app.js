ad0ceb629755db3018f92aff11704841e2a2719e
Fredrik Svensen <fs@bblpivotal.no>
2022-09-29 16:30:19 +0200

    storyblok

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import "../styles/globals.css";
+
+storyblokInit({
+  accessToken: "bAIGr0QLo25JJhu79kwXKQtt",
+  // bridge: false,
+  apiOptions: {
+    region: "eu", // Pass this key/value if your space was created under US region
+  },
+  use: [apiPlugin],
+  components: {
+    page: Page,
+    teaser: Teaser,
+    grid: Grid,
+    feature: Feature,
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
